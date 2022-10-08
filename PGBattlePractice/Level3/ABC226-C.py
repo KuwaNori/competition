28f57230d3881c3ef36235688e8e626964b41814
KuwaNori <kuwanori4778@gmail.com>
2022-10-08 23:34:43 +0900

    ABC226-C.py

--- PGBattlePractice/Level3/ABC226-C.py
+++ PGBattlePractice/Level3/ABC226-C.py
@@ -4,8 +4,7 @@ l = [INF for i in range(n)]
 for i in range(n):
     line = list(map(int, input().split()))
     t, k = line[0], line[1]
-    if k >= 1: s = l[line[-1]-1] + t
+    if k >= 1: s = sum([l[i-1] for i in line[2:k+2]]) + t
     else: s = t
     l[i] = s
-print(l)
 print(l[-1])
